0e5a02b26357be681dfe37b3fa7199d34581a32b
developertj <tejasbhanushali.1@gmail.com>
2025-03-24 16:38:25 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const taskRoutes = require("./routes/taskRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI; // Fetching MONGO_URI
+
+app.use(cors());
+app.use(express.json());
+app.use("/tasks", taskRoutes);
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Server is running...");
+});
+
+
+if (!MONGO_URI) {
+    console.error("Error: MONGO_URI is not defined. Check your .env file.");
+    process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("MonogoDB connected"))
+    .catch(err => console.log(err));
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
